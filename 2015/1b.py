6cd35e32f6efe52b05c0af800881bf3bad6d4809
Shrivatsa <shrivatsa.swadi@gmail.com>
2022-12-31 07:30:13 +0530

    A0C 2015 Day 1

---
+++ 2015/1b.py
@@ -0,0 +1,11 @@
+# What is the position of the character that causes Santa to first enter the basement?
+
+import pathlib
+content = pathlib.Path('1.input').read_text()
+
+counter = 0
+for idx, c in enumerate(content):
+  counter += 1 if c == '(' else -1
+  if counter == -1:
+    print(idx+1)
+    break
