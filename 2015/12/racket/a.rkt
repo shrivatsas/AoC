0185eaa8f6f4a757d526022cab323747681845d2
Shrivatsa <shrivatsa.swadi@gmail.com>
2023-01-18 16:15:54 +0530

    A0C 2015 Day 12

---
+++ 2015/12/racket/a.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(require json)
+
+(define input 
+  (port->string (open-input-file "../12.input") #:close?  #t))
+
+(define (sum-of-numbers-helper json-obj)
+  (cond
+    [(number? json-obj) json-obj]
+    [(list? json-obj) (apply + (map sum-of-numbers-helper json-obj))]
+    [(hash? json-obj) (apply + (map (lambda (key) (sum-of-numbers-helper (dict-ref json-obj key))) (dict-keys json-obj)))]
+    [else 0]))
+
+(define (sum-of-numbers input)
+  (define json-obj (string->jsexpr input))
+  (sum-of-numbers-helper json-obj))
+
+(display (sum-of-numbers input))
